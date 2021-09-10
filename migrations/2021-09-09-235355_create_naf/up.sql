1e7fdf33c391ef4ab2e580cd8f4734eee7fbbb60
Guileas <ethevegwenola@gmail.com>
2021-09-11 01:22:01 +0200

    feat: create migration

---
+++ migrations/2021-09-09-235355_create_naf/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+CREATE TABLE nafs (
+    uuid BINARY(16) PRIMARY KEY,
+    created_at TIMESTAMP(6) NOT NULL,
+    updated_at TIMESTAMP(6) NULL,
+    code VARCHAR(100) NOT NULL,
+    label VARCHAR(100) NOT NULL,
+    description VARCHAR(1000)
+);
+
