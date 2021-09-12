18bdcd30ca2b70b83b61483d737f523944c2eb08
Guileas <ethevegwenola@gmail.com>
2021-09-12 21:16:06 +0200

    feat: find naf by id

--- src/route/v1/naf/naf.rs
+++ src/route/v1/naf/naf.rs
@@ -47,6 +47,40 @@ pub fn get_all_naf(connection: Connection) -> Json<Vec<NafResource>> {
     Json(_nafs)
 }
 
+#[openapi]
+#[get("/v1/naf/<id>")]
+pub fn get_naf_by_id(connection: Connection, id: String) -> Json<Vec<NafResource>> {
+
+    let _id = Uuid::parse_str(&id).unwrap();
+    let naf = nafs::table
+        .filter(nafs::uuid.eq(&_id.as_bytes().to_vec()))
+        .limit(1)
+        .load::<Naf>(&*connection)
+        .expect("Error loading naf");
+
+    let default_uuid: Uuid = Uuid::parse_str("00000000000000000000000000000000").unwrap();
+    let _uuid = match Uuid::from_slice(naf[0].uuid.as_slice()) {
+        Ok(_uuid) => _uuid,
+        Err(_err) => default_uuid,
+    };
+
+    let _description = match naf[0].description{
+        None => "",
+        Some(ref x) => x,
+    };
+
+    let mut _nafs = Vec::new();
+
+    _nafs.push(NafResource{
+        uuid: _uuid.to_string(),
+        code: naf[0].code.to_string(),
+        label: naf[0].label.to_string(),
+        description: Some(_description.to_string()),
+    });
+
+    Json(_nafs)
+}
+
 
 #[openapi(tag = "Naf")]
 #[post("/v1/naf", format = "application/json", data = "<request>")]
