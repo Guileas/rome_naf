b72a1f8b21aed6a4545d31a910eda805c6689db7
Gwénola ETHEVE <ethevegwenola@gmail.com>
2023-04-21 18:16:03 +0200

    fix: label size from 100 to 255 char

--- migrations/2021-09-09-235355_create_naf/up.sql
+++ migrations/2021-09-09-235355_create_naf/up.sql
@@ -4,7 +4,7 @@ CREATE TABLE nafs (
     created_at TIMESTAMP(6) NOT NULL,
     updated_at TIMESTAMP(6) NULL,
     code VARCHAR(100) NOT NULL,
-    label VARCHAR(100) NOT NULL,
+    label VARCHAR(255) NOT NULL,
     description VARCHAR(1000)
 );
 
