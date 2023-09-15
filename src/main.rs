1cc13d73d61699b477e6f15acbc11d03aed1bc7f
guileas <ethevegwenola@gmail.com>
2023-09-15 19:12:44 +0200

    fix: remove deleted endpoint

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,6 @@ pub fn build_rocket() -> Rocket<Build> {
             route::v1::keyword::keyword::link_keyword_to_nafs,
             route::v1::keyword::keyword::get_nafs_by_keyword,
             route::v1::keyword::keyword::autocomplete_keyword_search,
-            route::v1::keyword::keyword::get_keyword_specialty,
             //Rome
             route::v1::rome::rome::insert_rome,
             route::v1::rome::rome::get_all_rome,
